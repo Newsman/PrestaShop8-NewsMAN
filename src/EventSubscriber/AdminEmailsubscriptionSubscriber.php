753fd66e279e8ebcffe5da667ed30a5d9c9a2c8a
Daniel Ifrim <daniel.ifrim7@gmail.com>
2026-04-07 10:08:48 +0300

    Add phpstan-ignore for isMasterRequest/isMainRequest Symfony compat

--- src/EventSubscriber/AdminEmailsubscriptionSubscriber.php
+++ src/EventSubscriber/AdminEmailsubscriptionSubscriber.php
@@ -49,6 +49,7 @@ class AdminEmailsubscriptionSubscriber implements EventSubscriberInterface
 
     public function onKernelRequest(RequestEvent $event): void
     {
+        /** @phpstan-ignore-next-line */
         if (!(method_exists($event, 'isMainRequest') ? $event->isMainRequest() : $event->isMasterRequest())) {
             return;
         }
